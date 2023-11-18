61c72272351122c77910a0cf40331081ef41c353
henry chun <henry.chun@cseducation.com.au>
2023-11-18 13:11:33 +1100

    added showing admin meny by only admin

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -2,12 +2,14 @@
 
 import Link from 'next/link';
 import { useParams, usePathname, useRouter } from 'next/navigation';
+import { useUser } from '@clerk/nextjs';
 import { cn } from '@/lib/utils';
 
 export default function MainNav() {
   const pathName = usePathname();
   const paramsValue = useParams();
   const params = paramsValue.param;
+  const { user } = useUser();
 
   const routes = [
     {
@@ -27,22 +29,38 @@ export default function MainNav() {
     },
   ];
 
+  // const canAccessAdmin = user?.publicMetadata?.accessible_paths?.includes('/admin');
+  // const canAccessAdmin = Array.isArray(user?.publicMetadata?.accessible_paths)
+  //   && user.publicMetadata.accessible_paths.includes('/admin');
+  const canAccessAdmin =
+    user &&
+    typeof user.publicMetadata === 'object' &&
+    Array.isArray(user.publicMetadata.accessible_paths) &&
+    user.publicMetadata.accessible_paths.includes('/admin');
+
   return (
     <nav className='flex items-center space-x-4 lg:space-x-6'>
-      {routes.map((route) => (
-        <Link
-          key={route.href}
-          href={route.href}
-          className={cn(
-            'text-sm font-medium transition-colors hover:text-primary',
-            route.active
-              ? 'text-black dark:text-white'
-              : 'text-muted-foreground'
-          )}
-        >
-          {route.label}
-        </Link>
-      ))}
+      {routes.map((route) => {
+        if (route.href === '/admin' && !canAccessAdmin) {
+          return null;
+        }
+
+        return (
+          <Link
+            key={route.href}
+            href={route.href}
+            className={cn(
+              'text-sm font-medium transition-colors hover:text-primary',
+              route.active
+                ? 'text-black dark:text-white'
+                : 'text-muted-foreground'
+            )}
+          >
+            {route.label}
+          </Link>
+        );
+      })}
+      {/* <AdminMenu /> */}
     </nav>
   );
 }
