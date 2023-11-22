6bdc02893ab6cafd004def167e9d8f5d548ef612
henry chun <henry.chun@cseducation.com.au>
2023-11-22 16:17:02 +1100

    fixed bug in responsive design

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,6 @@
 import Image from 'next/image';
 
 export default async function HomePage() {
-  const heroBackgroundImage = '/testbank-hero.svg';
-
   const items = [
     {
       type: 'Paperback',
@@ -22,69 +20,65 @@ export default async function HomePage() {
     <main className='min-h-screen'>
       {/* <main className='flex flex-col items-center justify-between min-h-screen'> */}
       {/* Hero Section */}
-      <section className='container relative px-8 py-12 mx-auto md:py-16'>
-        <div className='relative w-full min-h-[calc(100vh-64px)]'>
+      {/* Content Section */}
+      <section className='container flex flex-col-reverse items-center px-8 py-12 mx-auto bg-gradient-to-r from-yellow-900 to-yellow-400 md:py-16 md:flex-row '>
+        <div className='w-full px-4 md:flex-1 '>
+          <h1 className='mb-6 text-5xl font-bold text-white lg:text-6xl'>
+            <p>Bright Minds, Brighter Futures</p>
+            <p>Learning Made Fun!</p>
+          </h1>
+          <p className='mb-4 text-lg lg:text-xl text-zinc-300'>
+            Explore our wide selection of books, test papers, and audio books
+            designed for primary students. Stay ahead in learning and have fun!
+          </p>
+          <button className='px-6 py-2 text-white transition bg-black rounded-lg shadow-lg hover:bg-opacity-50'>
+            Shop
+          </button>
+        </div>
+        <div className='w-full mb-8 ml-6 md:mt-0 md:flex-1'>
           <Image
-            src={heroBackgroundImage}
-            layout='fill'
-            // width={1249}
-            // height={768}
-            className='object-cover object-center'
-            alt='Background'
-            priority // 페이지 로드 시 이미지가 우선적으로 로딩되도록 함
+            src='/hero_bookstore.png'
+            layout='responsive'
+            width={1024}
+            height={1024}
+            className='object-cover object-center rounded-lg shadow-2xl'
+            alt='Hero Image'
+            priority
           />
         </div>
-        {/* Content Section */}
-        <div className='absolute bottom-0 left-0 right-0 z-10 flex flex-col justify-center pl-4 top-16 md:pl-16 lg:pl-24'>
-          <div className='max-w-md'>
-            <h1 className='mb-6 text-4xl font-bold text-white lg:text-5xl'>
-              Bright Minds, Brighter Futures Learning Made Fun!
-            </h1>
-            <p className='mb-4 text-lg text-white lg:text-xl'>
-              Explore our wide selection of books, test papers, and audio books
-              designed for primary students. Stay ahead in learning and have
-              fun!
-            </p>
-            <button className='px-6 py-2 text-white transition bg-black rounded-lg shadow-lg hover:bg-opacity-70'>
-              Shop
-            </button>
-          </div>
-        </div>
       </section>
-      <section className='container px-8 py-12 mx-auto md:py-16'>
-        <div className='flex flex-col items-center md:flex-row'>
-          {/* Image content */}
-          <div className='mb-8 md:w-1/2 md:pr-8 md:mb-0'>
-            <Image
-              src='/section2_bookstore.png' // 실제 이미지 경로로 변경하세요
-              alt='Academic materials'
-              width={504} // 이미지의 실제 너비
-              height={298} // 이미지의 실제 높이
-              layout='responsive'
-              className='rounded-lg shadow-lg'
-            />
-          </div>
-          {/* Text content */}
-          <div className='md:w-1/2'>
-            <h2 className='mb-4 text-3xl font-bold'>
-              Unlock Academic Excellence: Prime Tests and Strategies
-            </h2>
-            <p className='mb-4'>
-              Welcome to our TestBank, online bookstore, where you can find a
-              wide range of educational materials specifically designed for
-              primary students. Elevate your child's learning experience with
-              our comprehensive exam preparation materials, perfect for primary
-              geniuses.
-            </p>
-            <ul className='mb-6 list-disc list-inside'>
-              <li>Ignite Your Passion for Learning with Strategy Books</li>
-              <li>Boost Academic Performance with Comprehensive Test Papers</li>
-              <li>Enhance Listening Skills with Captivating Audio Books</li>
-            </ul>
-            <button className='px-6 py-2 text-white transition duration-300 bg-black rounded-lg shadow-lg hover:bg-opacity-70'>
-              Shop
-            </button>
-          </div>
+      <section className='container flex flex-col items-center px-8 py-12 mx-auto md:py-16 md:flex-row'>
+        {/* Image content */}
+        <div className='w-full pr-8 mb-8 md:flex-1 md:mb-0'>
+          <Image
+            src='/section2_bookstore.png'
+            alt='Academic materials'
+            width={504}
+            height={298}
+            layout='responsive'
+            className='rounded-lg shadow-lg'
+          />
+        </div>
+        {/* Text content */}
+        <div className='w-full md:flex-1'>
+          <h2 className='mb-4 text-3xl font-bold'>
+            Unlock Academic Excellence: Prime Tests and Strategies
+          </h2>
+          <p className='mb-4'>
+            Welcome to our TestBank, online bookstore, where you can find a wide
+            range of educational materials specifically designed for primary
+            students. Elevate your child's learning experience with our
+            comprehensive exam preparation materials, perfect for primary
+            geniuses.
+          </p>
+          <ul className='mb-6 list-disc list-inside'>
+            <li>Ignite Your Passion for Learning with Strategy Books</li>
+            <li>Boost Academic Performance with Comprehensive Test Papers</li>
+            <li>Enhance Listening Skills with Captivating Audio Books</li>
+          </ul>
+          <button className='px-6 py-2 text-white transition duration-300 bg-black rounded-lg shadow-lg hover:bg-opacity-70'>
+            Shop
+          </button>
         </div>
       </section>
       <section className='container px-8 py-12 mx-auto'>
@@ -121,7 +115,7 @@ export default async function HomePage() {
       </section>
       <section className='container flex flex-col items-center px-8 py-12 mx-auto md:py-16 md:flex-row'>
         {/* Image container */}
-        <div className='flex-1'>
+        <div className='w-full md:flex-1'>
           <Image
             src='/section4_benefit_bookstore.png'
             alt='Educational Tools'
@@ -132,7 +126,7 @@ export default async function HomePage() {
           />
         </div>
         {/* Text content */}
-        <div className='flex-1 ml-4 md:ml-12'>
+        <div className='w-full mt-8 ml-4 md:flex-1 md:ml-12'>
           <h2 className='mb-4 text-4xl font-bold'>
             Transformative Educational Tools
           </h2>
@@ -154,10 +148,6 @@ export default async function HomePage() {
               learning curve.
             </p>
           </div>
-          <button className='px-6 py-2 text-white transition duration-300 bg-black rounded-lg shadow-lg hover:bg-opacity-70'>
-            Learn More
-          </button>
-          {/* 다른 버튼이 필요하면 여기에 추가 */}
         </div>
       </section>
     </main>
